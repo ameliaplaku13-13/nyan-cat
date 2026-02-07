d3ab01aaace6bbd78b0e02dd489f6eeeb69f0ce9
Amelia Plaku <ameliaplaku13@gmail.com>
2026-02-07 15:26:23 +0000

    I made the Nyan cat last a bit longer

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.LoopingInBackground)
-        for (let index = 0; index < 100; index++) {
+        for (let index = 0; index < 2000; index++) {
             basic.showIcon(IconNames.Heart)
             basic.showIcon(IconNames.SmallHeart)
         }
